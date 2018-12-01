8f210f42991be8037ee95d2749e5945876126e74
alex-bellon <alexrbellon@gmail.com>
2018-12-01 16:32:28 -0600

    Added ASCII robot, very important

--- Forensics/GIFAR/gifar.sh
+++ Forensics/GIFAR/gifar.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo -e "Flag: "
+printf "\n"
+echo -n "Flag: "
 read flag
 
 mkdir secret && cd secret && echo "text 15,15 "$flag"" > flag.txt
 
 convert -size 300x300 xc:white -font "FreeMono" -pointsize 12 -fill black -draw @flag.txt flag.png
 
-cd .. && zip -r secret secret
+cd .. && zip -r secret secret &> /dev/null
 
-echo -e "File to hide in: "
+echo -n "File to hide in: "
 read cover
 
 cat $cover secret.zip > $cover
 
-rm -rf secret-tmp
+rm -rf secret
 rm -f secret.zip
 
-echo "beep boop i'm done"
+printf "\n\t  ;    +---------------------+\n\t [\"] - | your file is ready! |\n\t/[_]\\  +---------------------+\n\t ] [  \n"
