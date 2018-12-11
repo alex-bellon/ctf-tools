e128219d86fb2a35f81d9832c115eb64f97ba6dd
alex-bellon <alexrbellon@gmail.com>
2018-12-10 18:14:44 -0600

    Added Affine cipher script

---
+++ Crypto/Affine/affine.py
@@ -0,0 +1,49 @@
+from fractions import gcd
+
+def encode():
+    a = input('What coprime number do you want to use for a? ')
+    while(gcd(int(a), 26) != 1):
+        a = input('Sorry, that number is not coprime with 26. Please enter a different number: ')
+    b = input('What number between 0-25 (inclusive) do you want to use for b? ')
+    while(int(b) > 25 or int(b) < 0):
+        b = input('Sorry, that number is not between 0-25. Please enter a number between 0-25 (inclusive): ')
+
+    plain = input('Input your message using only letters [A-Z]: ').lower()
+    a = int(a)
+    b = int(b)
+    cipher = ''
+    for letter in plain:
+        temp = chr((a * ord(letter) - 97 + b) % 26 + 97)
+        cipher += temp
+    print("\n\t  :\n\t [\"]  -[ " + cipher + " ]\n\t/[_]\\\n\t ] [")
+
+def decode():
+    a = input('What coprime number do you want to use for a? ')
+    while(gcd(int(a), 26) != 1):
+        a = input('Sorry, that number is not coprime with 26. Please enter a different number: ')
+    b = input('What number between 0-25 (inclusive) do you want to use for b? ')
+    while(int(b) > 25 or int(b) < 0):
+        b = input('Sorry, that number is not between 0-25. Please enter a number between 0-25 (inclusive): ')
+    a = int(a)
+    b = int(b)
+    cipher = input('Input your ciphertext: ').lower()
+    plain = ''
+    for letter in cipher:
+        temp = chr(int(((ord(letter) - 97 - b) / a) % 26 + 97))
+        plain += temp
+    print("\n\t  :\n\t [\"]  -[ " + plain + " ]\n\t/[_]\\\n\t ] [")
+
+
+def main():
+    while(1):
+        choice = input('\nEncode or decode? [e/d] ')
+        if choice == 'e' or choice == 'E':
+            encode()
+            break
+        elif choice == 'd' or choise == 'D':
+            decode()
+            break
+        else: 
+            print('Please choose [e] or [d]')
+
+main()
