0959beba2a06cf2b4d91faf6c59ec7ecd25defde
alex-bellon <alexrbellon@gmail.com>
2019-03-06 12:33:03 -0600

    Cleaning up forensics tools

--- Forensics/Whitespace/print-in-whitespace.py
+++ Forensics/Whitespace/print-in-whitespace.py
@@ -6,14 +6,14 @@ curr = ""
 for letter in plain:
     binary = "{0:b}".format(ord(letter))
     print(binary)
-    curr += "- - - - (0 "
+    curr += "1 0 1 1 10 "
     for bit in binary:
         if(bit == '1'):
-            curr += "1\t"
+            curr += "0\t"
         if(bit == '0'):
-            curr += "0 "
-    curr += ")\nprint\t\n"
+            curr += "1 "
+    curr += "0\n1\t\n"
 
-curr += "- - \n\n"
+curr += "1 1 \n\n"
 
 output.write(curr)
